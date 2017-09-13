f6192aa3ecfcedc82cbbb10b948c589d6c3db430
chayut  takaweekeaw <chayut-mac-mini@chayuts-Mac-mini.local>
2017-09-13 11:30:03 +0700

    trade history

---
+++ trade.js
@@ -0,0 +1,289 @@
+{
+"trades": [
+{
+"trade_id": "1711247",
+"rate": "132000.00000000",
+"amount": "0.01510616",
+"trade_date": "2017-09-13 11:22:25",
+"order_id": "5483144",
+"trade_type": "sell",
+"reference_id": "0",
+"seconds": 124
+},
+{
+"trade_id": "1711248",
+"rate": "131550.00000000",
+"amount": "0.33927344",
+"trade_date": "2017-09-13 11:22:25",
+"order_id": "5483132",
+"trade_type": "sell",
+"reference_id": "0",
+"seconds": 124
+},
+{
+"trade_id": "1711257",
+"rate": "133349.00000000",
+"amount": "0.01620675",
+"trade_date": "2017-09-13 11:22:28",
+"order_id": "5483068",
+"trade_type": "buy",
+"reference_id": "0",
+"seconds": 121
+},
+{
+"trade_id": "1711270",
+"rate": "132000.00000000",
+"amount": "0.06364050",
+"trade_date": "2017-09-13 11:22:46",
+"order_id": "5483146",
+"trade_type": "buy",
+"reference_id": "0",
+"seconds": 103
+},
+{
+"trade_id": "1711277",
+"rate": "132000.00000000",
+"amount": "1.21524601",
+"trade_date": "2017-09-13 11:22:53",
+"order_id": "5483146",
+"trade_type": "buy",
+"reference_id": "0",
+"seconds": 96
+},
+{
+"trade_id": "1711289",
+"rate": "132000.00000000",
+"amount": "0.01637245",
+"trade_date": "2017-09-13 11:23:00",
+"order_id": "5483146",
+"trade_type": "buy",
+"reference_id": "0",
+"seconds": 89
+},
+{
+"trade_id": "1711294",
+"rate": "132000.00000000",
+"amount": "0.00032358",
+"trade_date": "2017-09-13 11:23:18",
+"order_id": "5483146",
+"trade_type": "buy",
+"reference_id": "0",
+"seconds": 71
+},
+{
+"trade_id": "1711297",
+"rate": "132000.00000000",
+"amount": "0.01650817",
+"trade_date": "2017-09-13 11:23:31",
+"order_id": "5483146",
+"trade_type": "buy",
+"reference_id": "0",
+"seconds": 58
+},
+{
+"trade_id": "1711324",
+"rate": "132000.00000000",
+"amount": "0.01659855",
+"trade_date": "2017-09-13 11:24:03",
+"order_id": "5483146",
+"trade_type": "buy",
+"reference_id": "0",
+"seconds": 26
+},
+{
+"trade_id": "1711335",
+"rate": "132000.00000000",
+"amount": "0.29925000",
+"trade_date": "2017-09-13 11:24:20",
+"order_id": "5483146",
+"trade_type": "buy",
+"reference_id": "0",
+"seconds": 9
+}
+],
+"lowask": [
+{
+"order_id": "5483146",
+"rate": "132000.00000000",
+"amount": "1.99177628",
+"date_added": "2017-09-13 11:22:32",
+"order_type": "sell",
+"display_vol1": "262,914.47 THB",
+"display_vol2": "1.99177628 BTC"
+},
+{
+"order_id": "5483148",
+"rate": "133300.00000000",
+"amount": "0.20078080",
+"date_added": "2017-09-13 11:22:36",
+"order_type": "sell",
+"display_vol1": "26,764.08 THB",
+"display_vol2": "0.20078080 BTC"
+},
+{
+"order_id": "5483068",
+"rate": "133349.00000000",
+"amount": "0.50587376",
+"date_added": "2017-09-13 11:12:51",
+"order_type": "sell",
+"display_vol1": "67,457.76 THB",
+"display_vol2": "0.50587376 BTC"
+},
+{
+"order_id": "5483045",
+"rate": "133350.00000000",
+"amount": "0.07061705",
+"date_added": "2017-09-13 11:10:08",
+"order_type": "sell",
+"display_vol1": "9,416.78 THB",
+"display_vol2": "0.07061705 BTC"
+},
+{
+"order_id": "5483009",
+"rate": "133499.00000000",
+"amount": "0.15771528",
+"date_added": "2017-09-13 11:07:26",
+"order_type": "sell",
+"display_vol1": "21,054.83 THB",
+"display_vol2": "0.15771528 BTC"
+},
+{
+"order_id": "5482849",
+"rate": "133500.00000000",
+"amount": "0.10000000",
+"date_added": "2017-09-13 10:57:48",
+"order_type": "sell",
+"display_vol1": "13,350.00 THB",
+"display_vol2": "0.10000000 BTC"
+},
+{
+"order_id": "5483151",
+"rate": "133750.00000000",
+"amount": "0.02849552",
+"date_added": "2017-09-13 11:23:45",
+"order_type": "sell",
+"display_vol1": "3,811.28 THB",
+"display_vol2": "0.02849552 BTC"
+},
+{
+"order_id": "5482798",
+"rate": "133799.99000000",
+"amount": "0.00548463",
+"date_added": "2017-09-13 10:53:01",
+"order_type": "sell",
+"display_vol1": "733.84 THB",
+"display_vol2": "0.00548463 BTC"
+},
+{
+"order_id": "5483094",
+"rate": "133998.00000000",
+"amount": "0.07556761",
+"date_added": "2017-09-13 11:16:32",
+"order_type": "sell",
+"display_vol1": "10,125.91 THB",
+"display_vol2": "0.07556761 BTC"
+},
+{
+"order_id": "5482987",
+"rate": "134000.00000000",
+"amount": "1.10656117",
+"date_added": "2017-09-13 11:06:48",
+"order_type": "sell",
+"display_vol1": "148,279.20 THB",
+"display_vol2": "1.10656117 BTC"
+}
+],
+"highbid": [
+{
+"order_id": "5483132",
+"rate": "131550.00",
+"amount": "0.90992018",
+"date_added": "2017-09-13 11:20:20",
+"order_type": "buy",
+"display_vol1": "119,700.00 THB",
+"display_vol2": "0.90992018 BTC"
+},
+{
+"order_id": "5483023",
+"rate": "131500.00",
+"amount": "4.76017943",
+"date_added": "2017-09-13 11:08:40",
+"order_type": "buy",
+"display_vol1": "625,963.60 THB",
+"display_vol2": "4.76017943 BTC"
+},
+{
+"order_id": "5483058",
+"rate": "131500.00",
+"amount": "0.00037928",
+"date_added": "2017-09-13 11:11:29",
+"order_type": "buy",
+"display_vol1": "49.88 THB",
+"display_vol2": "0.00037928 BTC"
+},
+{
+"order_id": "5483134",
+"rate": "131500.00",
+"amount": "0.01132879",
+"date_added": "2017-09-13 11:20:39",
+"order_type": "buy",
+"display_vol1": "1,489.74 THB",
+"display_vol2": "0.01132879 BTC"
+},
+{
+"order_id": "5483156",
+"rate": "131500.00",
+"amount": "0.04551331",
+"date_added": "2017-09-13 11:23:56",
+"order_type": "buy",
+"display_vol1": "5,985.00 THB",
+"display_vol2": "0.04551331 BTC"
+},
+{
+"order_id": "5483069",
+"rate": "131491.00",
+"amount": "1.92606629",
+"date_added": "2017-09-13 11:12:58",
+"order_type": "buy",
+"display_vol1": "253,260.38 THB",
+"display_vol2": "1.92606629 BTC"
+},
+{
+"order_id": "5483024",
+"rate": "131490.00",
+"amount": "0.31908014",
+"date_added": "2017-09-13 11:08:45",
+"order_type": "buy",
+"display_vol1": "41,955.85 THB",
+"display_vol2": "0.31908014 BTC"
+},
+{
+"order_id": "5483124",
+"rate": "131450.00",
+"amount": "0.00197299",
+"date_added": "2017-09-13 11:19:58",
+"order_type": "buy",
+"display_vol1": "259.35 THB",
+"display_vol2": "0.00197299 BTC"
+},
+{
+"order_id": "5482319",
+"rate": "131310.00",
+"amount": "0.15218883",
+"date_added": "2017-09-13 10:18:09",
+"order_type": "buy",
+"display_vol1": "19,983.92 THB",
+"display_vol2": "0.15218883 BTC"
+},
+{
+"order_id": "5477833",
+"rate": "131300.00",
+"amount": "0.75971059",
+"date_added": "2017-09-13 02:43:30",
+"order_type": "buy",
+"display_vol1": "99,750.00 THB",
+"display_vol2": "0.75971059 BTC"
+}
+],
+"user_orders": []
+}
