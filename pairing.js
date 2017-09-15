d152ff35802b0ee5727513281e6f2f950af100da
chayut takaweekaew <newcup1002@gmail.com>
2017-09-15 11:03:37 +0700

    Pairing list of crypto

---
+++ pairing.js
@@ -0,0 +1,107 @@
+{
+	"1": {
+		"pairing_id": 1,
+		"primary_currency": "THB",
+		"secondary_currency": "BTC"
+	},
+	"2": {
+		"pairing_id": 2,
+		"primary_currency": "BTC",
+		"secondary_currency": "LTC"
+	},
+	"3": {
+		"pairing_id": 3,
+		"primary_currency": "BTC",
+		"secondary_currency": "NMC"
+	},
+	"4": {
+		"pairing_id": 4,
+		"primary_currency": "BTC",
+		"secondary_currency": "DOG"
+	},
+	"5": {
+		"pairing_id": 5,
+		"primary_currency": "BTC",
+		"secondary_currency": "PPC"
+	},
+	"6": {
+		"pairing_id": 6,
+		"primary_currency": "BTC",
+		"secondary_currency": "FTC"
+	},
+	"7": {
+		"pairing_id": 7,
+		"primary_currency": "BTC",
+		"secondary_currency": "XPM"
+	},
+	"8": {
+		"pairing_id": 8,
+		"primary_currency": "BTC",
+		"secondary_currency": "ZEC"
+	},
+	"13": {
+		"pairing_id": 13,
+		"primary_currency": "BTC",
+		"secondary_currency": "HYP"
+	},
+	"14": {
+		"pairing_id": 14,
+		"primary_currency": "BTC",
+		"secondary_currency": "PND"
+	},
+	"15": {
+		"pairing_id": 15,
+		"primary_currency": "BTC",
+		"secondary_currency": "XCN"
+	},
+	"17": {
+		"pairing_id": 17,
+		"primary_currency": "BTC",
+		"secondary_currency": "XPY"
+	},
+	"19": {
+		"pairing_id": 19,
+		"primary_currency": "BTC",
+		"secondary_currency": "QRK"
+	},
+	"20": {
+		"pairing_id": 20,
+		"primary_currency": "BTC",
+		"secondary_currency": "ETH"
+	},
+	"21": {
+		"pairing_id": 21,
+		"primary_currency": "THB",
+		"secondary_currency": "ETH"
+	},
+	"22": {
+		"pairing_id": 22,
+		"primary_currency": "THB",
+		"secondary_currency": "DAS"
+	},
+	"23": {
+		"pairing_id": 23,
+		"primary_currency": "THB",
+		"secondary_currency": "REP"
+	},
+	"24": {
+		"pairing_id": 24,
+		"primary_currency": "THB",
+		"secondary_currency": "GNO"
+	},
+	"25": {
+		"pairing_id": 25,
+		"primary_currency": "THB",
+		"secondary_currency": "XRP"
+	},
+	"26": {
+		"pairing_id": 26,
+		"primary_currency": "THB",
+		"secondary_currency": "OMG"
+	},
+	"27": {
+		"pairing_id": 27,
+		"primary_currency": "THB",
+		"secondary_currency": "BCH"
+	}
+}
