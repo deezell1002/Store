e64db294fd1791d8b6a0dfaec4978982ade9001a
chayut takaweekaew <newcup1002@gmail.com>
2017-09-21 16:03:22 +0700

    helpcenter for shopbank

---
+++ helpcenter.js
@@ -0,0 +1,38 @@
+[
+	{
+		"userId": 1,
+		"id": 1,
+		"title": "Can I transfer funds between accounts?",
+		"body": "Yes. You can transfer from any eligible checking, money market, savings, or personal line of credit account you have linked to your profile. You can transfer money to any checking, money market, savings, personal line of credit, and most personal loan accounts. (In cases where account types are IRAs or other restricted products, transfers may not be permitted."
+	},
+	{
+		"userId": 1,
+		"id": 2,
+		"title": "I entered wrong password 3 times and it is disabled / locked",
+		"body": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+	},
+	{
+		"userId": 1,
+		"id": 3,
+		"title": "What’s the difference between transferring funds from my credit card and requesting a direct deposit from my credit card?",
+		"body": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+	},
+	{
+		"userId": 1,
+		"id": 4,
+		"title": "How do I apply for a savings account?",
+		"body": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+	},
+	{
+		"userId": 1,
+		"id": 5,
+		"title": "Where do I go to find out about my rewards or to find out more about a rewards program?",
+		"body": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+	},
+	{
+		"userId": 1,
+		"id": 6,
+		"title": "dolorem eum magni eos aperiam quia",
+		"body": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+	}
+]
